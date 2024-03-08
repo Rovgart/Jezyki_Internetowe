7c53a028203bd6a0a89f592c5c1a22ebcff908d2
Daniel Niedzielski <daniel.niedziels@wp.pl>
2024-03-08 18:51:02 +0100

    Still a lot of work

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,21 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
 
   theme: {
-    fontSize:{
-      'nav_clamp':`clamp(".5rem,2vw,1.3rem")`,
-      'rsm':'10px',
+    fontSize: {
+      nav_clamp: `clamp("3rem", "2vw", "5rem")`,
+      rsm: "10px",
     },
-    gridTemplateColumns:{
-      "layout":'minmax(0,10rem) minmax(0,1fr) minmax(0,1fr) minmax(0,10rem)',
-
+    fontFamily: {
+      helvetica: `Helvetica`,
     },
-    extend: {
-
+    gridTemplateColumns: {
+      layout: "minmax(0,10rem) minmax(0,1fr) minmax(0,1fr) minmax(0,10rem)",
+      companies: "minmax(0,1fr) minmax(0,1fr)",
     },
+    gridTemplateRows: {
+      companies: "minmax(0,1fr) minmax(0,1fr)",
+    },
+    extend: {},
   },
   plugins: [],
-}
-
+};
